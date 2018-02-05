4248e4aaa580662ad31776cabacb85f6252a5a83
gmluo <gmluo@Ctrip.com>
2018-02-05 19:09:14 +0800

    添加侧边栏功能模块按钮

--- src/components/common/SiderBar.vue
+++ src/components/common/SiderBar.vue
@@ -33,142 +33,55 @@
 						index: 'searchsource',
 						title: '搜索报文',
 						subs: [{
-								index: 'sourceoperation',
-								title: '查看报文'
-						},{
-								index: 'getlogsource',
-								title: '获取报文'
-							}
-						]
+							index: 'sourceoperation',
+							title: '查看报文'
+						}, {
+							index: 'getlogsource',
+							title: '获取报文'
+						}]
 					},
-					
-//					{
-//						icon: 'el-icon-menu',
-//						index: 'table',
-//						title: '表格',
-//						subs: [
-//							{
-//								index: 'extendbutton',
-//								title: '门票接口'
-//							}, {
-//								index: 'basebutton',
-//								title: '玩乐接口'
-//							}, {
-//								index: 'basebutton',
-//								title: '联想词接口接口'
-//							}
-//						]
-//					},
-//					{
-//						icon: 'el-icon-date',
-//						index: 'form',
-//						title: '表单',
-//						subs: [{
-//								index: 'baseform',
-//								title: '典型表单'
-//							},
-//							{
-//								index: 'inlineform',
-//								title: '行内表单'
-//							},
-//							{
-//								index: 'positionfrom',
-//								title: '对齐方式'
-//							},
-//							{
-//								index: 'rulesform',
-//								title: '表单验证'
-//							},
-//							{
-//								index: 'selfrulesform',
-//								title: '自定义校验规则'
-//							},
-//							{
-//								index: 'additemform',
-//								title: '动态增减表单项'
-//							},
-//							{
-//								index: 'numberrulesform',
-//								title: '数字类型验证'
-//							},
-//							{
-//								index: 'sizeform',
-//								title: '组件尺寸控制'
-//							}
-//						]
-//					},
-//					{
-//						icon: 'el-icon-menu',
-//						index: 'collapse',
-//						title: '折叠面板',
-//						subs: [{
-//								index: 'basecollapse',
-//								title: '基础用法'
-//							},
-//							{
-//								index: 'extendcollapse',
-//								title: '扩展用法'
-//							}
-//						]
-//					},
-//					{
-//						icon: 'el-icon-document',
-//						index: 'upload',
-//						title: '文件上传',
-//						subs: [{
-//								index: 'baseupload',
-//								title: '基本样式'
-//							},
-//							{
-//								index: 'imgupload',
-//								title: '上传图像'
-//							},
-//							{
-//								index: 'imgwall',
-//								title: '照片墙'
-//							},
-//							{
-//								index: 'imglist',
-//								title: '图片列表缩略图'
-//							},
-//							{
-//								index: 'dragupload',
-//								title: '拖拽上传'
-//							},
-//							{
-//								index: 'manualupload',
-//								title: '手动上传'
-//							},
-//							{
-//								index: 'imguploadsize',
-//								title: '支持图片裁剪'
-//							}
-//						]
-//					},
-//					{
-//						icon: 'el-icon-document',
-//						index: 'editor',
-//						title: '编辑器',
-//						subs: [{
-//								index: 'vueeditor',
-//								title: 'VueEditor'
-//							},
-//							{
-//								index: 'markdownditor',
-//								title: 'MarkdownEditor'
-//							}
-//						]
-//					},
-//					{
-//						icon: 'el-icon-star-on',
-//						index: 'basecharts',
-//						title: '图表'
-//					},
-//					{
-//						icon: 'el-icon-upload2',
-//						index: 'drag',
-//						title: '拖拽'
-//					}
+					{
+						icon: 'el-icon-menu',
+						index: 'testreport',
+						title: '测试报告',
+						subs: [{
+							index: 'autotestreport',
+							title: '自动化测试报告'
+						}]
+					},
+					{
+						icon: 'el-icon-menu',
+						index: 'apitest',
+						title: '接口测试',
+						subs: [{
+							index: 'searchapitest',
+							title: '搜索接口测试'
+						}]
+					},
+					{
+						icon: 'el-icon-menu',
+						index: 'apptest',
+						title: 'app测试',
+						subs: [{
+							index: 'androidtest',
+							title: '安卓测试'
+						}, {
+							index: 'iostest',
+							title: 'IOS测试'
+						}]
+					},
+					{
+						icon: 'el-icon-menu',
+						index: 'webtest',
+						title: 'web测试',
+						subs: [{
+							index: 'webtest',
+							title: 'WEB测试'
+						}, {
+							index: 'h5test',
+							title: 'H5测试'
+						}]
+					},
 				]
 			}
 		},
